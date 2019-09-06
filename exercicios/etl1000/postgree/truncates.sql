545b63b02f5091d3bb62148b742842dd81ccc0f6
Rodolfo Lima <rodoxlima@gmail.com>
2019-09-06 16:30:49 -0400

    Etl 1000 e 2000 com Postgres

---
+++ exercicios/etl1000/postgree/truncates.sql
@@ -0,0 +1,20 @@
+--truncate table public.stage_pais_territorio;
+--truncate table public.stage_territorio;
+--select * from public.stage_pais_territorio;
+--select * from public.stage_territorio;
+
+--truncate table public.tbl_ft_vnds;
+--truncate table public.tbl_dmns_vnddr CASCADE;
+--truncate table public.tbl_dmns_clnt CASCADE;
+
+--select * from public.tbl_ft_vnds;
+--select * from public.tbl_dmns_vnddr --19;
+--select * from public.tbl_dmns_clnt --122;
+
+
+--ALTER SEQUENCE tbl_ft_vnds_id_seq RESTART;
+--UPDATE tbl_ft_vnds SET id = DEFAULT;
+
+--ALTER SEQUENCE tbl_dmns_clnt_clienteid_seq RESTART;
+--UPDATE tbl_dmns_clnt SET clienteid = DEFAULT;
+
